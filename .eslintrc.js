94b63bfc00a154203482a329c348bac2f449a9e1
Bendwarn <bendwarn@gmail.com>
2019-11-04 09:13:47 +0800

    init a runnable vue

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    extends: [
+        'standard',
+        "plugin:vue/base"
+    ],
+    parser: 'vue-eslint-parser',
+    parserOptions: {
+        parser: '@typescript-eslint/parser',
+    }
+}

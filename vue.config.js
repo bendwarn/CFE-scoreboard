bb82757b3c16d4bc43ec3a6821506da3251dc927
Bendwarn <bendwarn@gmail.com>
2019-11-22 13:39:40 +0800

    try index.html symlinks

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-  chainWebpack: config => {
-    config
-      .plugin('html')
-      .tap(args => {
-        if (process.env.NODE_ENV == 'production') {
-          args[0].filename = '../index.html'
-        }
-        return args
-      })
-  },
+  // chainWebpack: config => {
+  //   config
+  //     .plugin('html')
+  //     .tap(args => {
+  //       if (process.env.NODE_ENV == 'production') {
+  //         args[0].filename = '../index.html'
+  //       }
+  //       return args
+  //     })
+  // },
   publicPath: process.env.NODE_ENV == 'production'
-    ? '/CFE-scoreboard/'
+    ? '/CFE-scoreboard/dist/'
     : '/'
 }

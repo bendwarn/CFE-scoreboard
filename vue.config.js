5fad2e9ff5b67e89d1de1bbe83b0b74e7332a126
Bendwarn <bendwarn@gmail.com>
2019-11-22 12:27:14 +0800

    want index.html to work at root

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 module.exports = {
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        if (process.env.NODE_ENV == 'production') {
+          args[0].filename = '../index.html'
+        }
+        return args
+      })
+  },
   publicPath: process.env.NODE_ENV == 'production'
     ? '/CFE-scoreboard/dist/'
     : '/'

133c439fc86943692ea601a4df6bc4126e8c911d
Bendwarn <bendwarn@gmail.com>
2022-05-25 08:04:55 +0800

    fix: tap makes clickOutside flash

--- components/Dashboard.vue
+++ components/Dashboard.vue
@@ -63,7 +63,6 @@ const show = autoResetRef(false, 2000)
 
 let pinchActivated = countChange.initial
 onMounted(() => {
-  const clickType = navigator.maxTouchPoints ? 'tap' : 'click'
   interact(unrefElement(hpref))
     .draggable(true)
     .dropzone({
@@ -76,7 +75,7 @@ onMounted(() => {
       },
       ondrop: hp.swap,
     })
-    .on(clickType, () => {
+    .on('click', () => {
       emit('reqCal', hp[props.pos], 'hp', (payload: string) => {
         if (isNaN(+payload)) {
           show.value = true
@@ -111,7 +110,7 @@ onMounted(() => {
   shieldrefs.value.forEach((ele, i) => {
     interact(ele.firstElementChild as HTMLDivElement)
       .draggable(true)
-      .on(clickType, () => {
+      .on('click', () => {
         emit('reqCal', shield[props.pos][i], 'sp', (sh: string) => {
           if (isNaN(+sh)) {
             show.value = true
